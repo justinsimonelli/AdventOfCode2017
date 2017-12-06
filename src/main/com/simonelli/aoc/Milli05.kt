87d1519a7011623571ec7ae70f915aa835a0a0b5
Justin Simonelli <jsimonelli@expedia.com>
2017-12-06 14:59:04 -0800

    Minor day 5 update

--- src/main/com/simonelli/aoc/Milli05.kt
+++ src/main/com/simonelli/aoc/Milli05.kt
@@ -4,15 +4,12 @@ fun main(args: Array<String>) {
     val puzzleInput = FileReader().getPuzzleInput("src/main/resources/input/milli05.txt")
 
     val instructions = puzzleInput.split("\n").map { it.toInt() }
-    var instruction: Int = 0
-
-    var numSteps = callInstruction(instructions)
-    println("number of steps to escape = $numSteps")
+    println("Number of steps to escape = ${callInstruction(instructions)}")
 }
 
 fun callInstruction(instructions: List<Int>): Int {
 
-    var mutableList = instructions.toMutableList()
+    val mutableList = instructions.toMutableList()
     var index = 0
     var numSteps = 0
 
